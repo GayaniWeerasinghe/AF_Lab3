3ad699fcb74b0a2e4ecb1a5f451eea4ebc6b8c33
Gayani Weerasinghe <IT20656256@my.sliit.lk>
2023-03-04 22:29:58 +0530

    add AsyncAwait file

---
+++ asyncAwait.js
@@ -0,0 +1,12 @@
+const myPromise = require("./promises");
+
+async function myFunction() {
+    try {
+    const result = await myPromise;
+    console.log(result);
+    } catch (error) {
+    console.log(error);
+    }
+   }
+   myFunction();
+   
